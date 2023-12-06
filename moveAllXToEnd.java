5cd13976317da622b0cee4dd69deb697ebd42b44
ADMIN <yathisha.15177@gmail.com>
2023-12-06 17:29:47 +0530

    move all x to the end of the string by taking string and element by user

--- moveAllXToEnd.java
+++ moveAllXToEnd.java
@@ -1,24 +1,30 @@
+import java.util.*;
 public class moveAllXToEnd {
-    public static void moveAllX(String str , int idx , int count , String newString){
+    public static void moveAllX(String str , int idx , int count , String newString , String element){
         if(idx == str.length()){
             for(int i=0;i<count;i++){
-                newString += 'x' ;
+                newString += element ;
             }
             System.out.println(newString);
             return ;
         }
 
         char currChar = str.charAt(idx);
-        if(currChar == 'x'){
+        char currChar2 = element.charAt(0);
+        if(currChar == currChar2){
             count ++ ;
-            moveAllX(str, idx+1, count, newString);
+            moveAllX(str, idx+1, count, newString,element);
         } else {
             newString += currChar ;
-            moveAllX(str, idx+1, count, newString);
+            moveAllX(str, idx+1, count, newString,element);
         }
     }
     public static void main(String[] args) {
-        String str = "abcxdef" ;
-        moveAllX(str,0,0,"");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the string to move the elements to the end");
+        String str = sc.next();
+        System.out.println("enter the element you want to move to the last ");
+        String element = sc.next();
+        moveAllX(str,0,0,"",element);
     }
 }
